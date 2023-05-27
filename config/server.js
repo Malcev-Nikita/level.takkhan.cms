c65235dbe06ef4aa7ce86d22a61c08219c6355e5
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-05-27 21:46:04 +0400

    commit 27.5.23 (1)

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '236d1ad6811cf18e1cf50893255baef4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '75267312-bb16-45fb-9f2e-ee221e5d48ca'
+};
